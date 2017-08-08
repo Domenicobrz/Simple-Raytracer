eb55791855a9fe8d7dfed885c966ac4c0a0a4e91
Domenicobrz <domenicobrz@live.it>
2017-08-08 16:04:22 +0200

    Source.cpp added

---
+++ Raytracer/Source.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#include "Display.h"
+
+
+const int WIDTH  = 600;
+const int HEIGHT = 600;
+
+// extern is redundant, function declarations have external linkage by default
+//extern int kerneltest2();
+extern void freeCUDAResources();
+
+int main() {
+	if (!glfwInit()) {
+		fprintf(stderr, "failed to initialize GLFW");
+		return -1;
+	}
+	else {
+		fprintf(stderr, "GLFW initialized");
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 1); // 4x antialiasing
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL 
+
+	// Open a window and create its OpenGL context
+	GLFWwindow* window; // (In the accompanying source code, this variable is global)
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Tutorial 01", NULL, NULL);
+	if (window == NULL){
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window); // Initialize GLEW
+	glewExperimental = true; // Needed in core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		return -1;
+	}
+
+
+
+	Display displayProgram = Display(WIDTH, HEIGHT);
+
+
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	do{
+		displayProgram.update();
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+
+
+	return 0;
+}
